735555a4525ed1f17e89a24b56162596b5aa1da3
Kas Perch <the@nodebotani.st>
2017-11-29 01:08:51 -0600

    string to int

--- index.py
+++ index.py
@@ -3,4 +3,4 @@ import Adafruit_GPIO.Platform as Platform
 platform = Platform.platform_detect()
 
 print("Diagnostics:\n")
-print("Platform: " + platform)
+print("Platform is RPi: " + platform == 1)
