43dc9130c2f74a060c17c18b05f150d15931823f
Kas Perch <the@nodebotani.st>
2017-11-29 08:27:12 -0600

    Python scoping is really weird

--- blink.py
+++ blink.py
@@ -8,15 +8,15 @@ GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(LED_PIN, GPIO.OUT)
 
+def loop():
+  global LED_STATE
+  GPIO.output(LED_PIN, LED_STATE)
+  LED_STATE = not LED_STATE
+  time.sleep(500)
+  
 if __name__ == '__main__':
   try:
     while True:
       loop()
   finally:
     GPIO.cleanup()
-
-def loop():
-  global LED_STATE
-  GPIO.output(LED_PIN, LED_STATE)
-  LED_STATE = not LED_STATE
-  time.sleep(500)
