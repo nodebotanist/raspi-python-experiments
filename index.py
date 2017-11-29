89f6299d0f11afa478359d2f66317d4f91376bcf
Kas Perch <the@nodebotani.st>
2017-11-29 01:09:51 -0600

    Ugh

--- index.py
+++ index.py
@@ -3,4 +3,4 @@ import Adafruit_GPIO.Platform as Platform
 platform = Platform.platform_detect()
 
 print("Diagnostics:\n")
-print("Platform is RPi: " + platform == 1)
+print("Platform is RPi: {0}", platform == 1)
